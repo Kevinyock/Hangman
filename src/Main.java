4b231fae544c950b9e340bc241bb6aa800885bac
KevinTheGreat <Westminsterkevindo@gmail.com>
2021-07-31 22:46:54 -0700

    Playable Demo

--- src/Main.java
+++ src/Main.java
@@ -11,22 +11,30 @@ import java.util.Scanner;
 public class Main {
 
 	public static void main(String[] args) {
+		
+		int randNum;
+		File VeryEZFile = new File("VeryEZWord.txt");
+		File EZFile = new File("EZWord.txt");
+		File NormalFile = new File("NormWord.txt");
+		File HardFile = new File("HardWord.txt");
+		File VeryHardFile = new File("VeryHard.txt");
+		
+		boolean wordGussed = false;
+		
+		String Chosenword = "Tesla";
+		char[] Userguessword;
+		char guessletter;
 
-		/**
-		 * 
-		 */
+		int difficultyInput = 0;
+		
+		Scanner WordScanner;
+		Scanner scanner = new Scanner(System.in);
+		
 		try {
-			Scanner wordfile = new Scanner(new File("words.txt"));
+			WordScanner = new Scanner(new File("veryEZwords.txt"));
 		} catch (FileNotFoundException e1) {
 			System.out.println("File not found");
 		}
-		
-		Scanner scanner = new Scanner(System.in);
-		
-		String Chosenword;
-		char[] Userguessword;
-		
-		int difficultyInput = 0;
 		
 		try {
 			while (difficultyInput == 0) {
@@ -38,6 +46,8 @@ public class Main {
 				System.out.println("5 - Very Hard");
 
 				difficultyInput = scanner.nextInt();
+				
+				/*
 				switch (difficultyInput) {
 				case 1:
 					break;
@@ -53,10 +63,41 @@ public class Main {
 					System.out.println("Please input one of the proper difficulty");
 					break;
 				}
+				*/
 
 			}
 		} catch (InputMismatchException e) {
 			scanner.nextLine();
+		}
+		
+		Userguessword = new char[Chosenword.length()];
+		
+		for(int i = 0; i < Chosenword.length(); i++)
+		{
+			Userguessword[i] = '_';
+		}
+		
+		while(!wordGussed)
+		{
+			
+			for(int i = 0; i < Chosenword.length(); i++)
+			{
+				System.out.print(Userguessword[i] + " ");
+			}
+			System.out.println();
+			System.out.println("What is the next letter you chose");
+			
+			guessletter = scanner.next().charAt(0);
+			System.out.println(guessletter);
+			
+			for(int i = 0; i < Chosenword.length(); i++)
+			{
+				if (Chosenword.charAt(i) == guessletter)
+				{
+					Userguessword[i] = Chosenword.charAt(i);
+				}
+			}
+			
 		}
 	}
 
